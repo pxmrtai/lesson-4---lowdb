df014759e953848f33395b9fb5f298615d9c688c
Glitch (trapezoidal-precious-pisces) <none>
2020-08-06 04:05:34 +0000

    🕌⏳ Checkpoint ./server.js:20086806/671 ./db.json:20086806/176

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express'); 
 const app = express(); 
 const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+ 
+// Set some defaults
+db.defaults({ todos: [] })
+  .write()
 
 app.set('views', './views'); // Thư mục views nằm cng cấp với file app.js
 app.set('view engine', 'pug'); // Sử dụng pug làm view engine
@@ -9,22 +18,22 @@ app.set('view engine', 'pug'); // Sử dụng pug làm view engine
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-var todos =[
-            {name: 'đi chợ'},
-            {name: 'nấu cơm'},
-            {name: 'rửa bát'},
-            {name: 'học coderX'}  
-        ]
+// var todos =[
+//             {name: 'đi chợ'},
+//             {name: 'nấu cơm'},
+//             {name: 'rửa bát'},
+//             {name: 'học coderX'}  
+//         ]
 
         
 app.get('/',function(req, res){
     res.render('todos/index',{
-        todos: todos
+        todos: db.get('todos').value()
     })
 })
 app.get('/todos',function(req,res){
   var q = req.query.q;
-  var matched = todos.filter(function(todo){
+  var matched = db.get('todos').filter(function(todo){
     return todo.name.indexOf(q) !== -1; 
   })
   res.render('todos/index',{
@@ -35,7 +44,7 @@ app.get('/todos/create',function(req,res){
     res.render('todos/create')
 })
 app.post('/todos/create',function(req,res){
-    todos.push(req.body);
+    db.get('todos').push(req.body).write();
     res.redirect('back')
 })
 
